c8317d943849b0c50255ac8a4215431cb21b4e6f
Raminderpreet90 <kaurr90@student.douglascollege.ca>
2019-11-05 21:31:23 -0800

    email attribute is added in user.javaclass

--- src/main/java/com/Rent/bean/user.java
+++ src/main/java/com/Rent/bean/user.java
@@ -3,6 +3,13 @@ package com.Rent.bean;
 public class user {
 	private String name;
 	private String password;
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public String getName() {
 		return name;
 	}
